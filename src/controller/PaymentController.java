f25f6a5fcb1397f438f9a1c50a63af43573309d7
Soumyakanti Giri <giri.soumyakanti@gmail.com>
2024-01-22 21:43:26 +0530

    feature - payment enhancer points

--- src/controller/PaymentController.java
+++ src/controller/PaymentController.java
@@ -6,8 +6,13 @@ import model.Receipt;
 
 public class PaymentController {
     public void generateBill(String ticketId, int gateId) {
+        //get Gate
+        //get Ticket
+        //generateBill
     }
 
     public void postPayment(Bill bill, PaymentMode paymentMode) {
+        //getConfirmationFromThirdParty
+        //generateReceipt
     }
 }
